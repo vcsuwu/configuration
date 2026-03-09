124a20183081a42902eb5c8c611058894996451d
hollow <placeholder>
2026-03-09 22:22:10 +0500

    [FEATURE] added module folder

---
+++ modules/nix/nextcloud.nix
@@ -0,0 +1,19 @@
+{
+  config,
+  lib,
+  pkgs,
+}:
+{
+  environment.etc."nextcloud-admin-pass".text = "123456";
+  services.nextcloud = {
+    enable = true;
+    package = pkgs.nextcloud33;
+    hostName = "localhost";
+    config.adminpassFile = "/etc/nextcloud-admin-pass";
+    config.dbtype = "sqlite";
+    settings.trusted_domains = [
+      "192.168.0.12"
+      "localhost"
+    ];
+  };
+}
